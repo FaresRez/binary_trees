9dd3654a5f026165fc72abbf4c432a89eb016aab
FaresRez <fers.rez16@gmail.com>
2024-01-04 09:12:17 -0800

    Separate declaration

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - create a right node
+ *
+ * @parent: pointer of the parent node
+ * @value: the value of the node
+ * Return: NULL or the pointer to a node
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+
+        if (parent == NULL)
+                return (NULL);
+        binary_tree_t *node = binary_tree_node(parent, value);
+        if (node == NULL)
+                return (NULL);
+        if (parent->right != NULL)
+        {
+                node->right = parent->right;
+                parent->right->parent = node;
+
+        }
+        parent->right = node;
+        return (node);
+}
