20037e74ac6a2cf86ba371ab1f61f63ba62b56a9
FaresRez <fers.rez16@gmail.com>
2024-01-04 03:16:52 -0800

    Add the condition node is NULL

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,7 +15,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 	binary_tree_t *node = binary_tree_node(parent, value);
-
+	if (node == NULL)
+		return (NULL);
 	if (parent->left != NULL)
 	{
 		node->left = parent->left;
